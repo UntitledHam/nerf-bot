20823f99bd8d2536a04a65dfb18c2264056a2cb8
untitledham <namelessham@gmail.com>
2024-02-14 20:15:43 -0500

    Implemented date functionality.

--- main.py
+++ main.py
@@ -42,11 +42,19 @@ def convert_to_24_hour_time(date: str):
 		raise ValueError("Invalid date input.")
 
 def convert_date(text: str):
+	
 	split_text = text.split(" ")
 	split_text = split_text[1:-1]
 	del split_text[2]
 	split_text[2] = convert_to_24_hour_time(split_text[2])
 	split_text[0] = month_to_num(split_text[0])
+
+	# If a future event has a lesser month it has to be next year.
+	today = datetime.now()
+	if today.month > split_text[0]:
+		year = today.year + 1
+	else: year = today.year
+	split_text = [year] + split_text
 	
 	output = ""
 	for text in split_text:
@@ -66,21 +74,23 @@ def convert_text_to_list(text: str):
 	return list_text
 
 def print_events(events):
-	for i in range(len(events)):
-		print(f"""Name: {events[i]["name"]}\nDate: {events[i]["date"]}\nLocation: {events[i]["location"]}\n\n""")
+	for date, event in events.items():
+		print(f"""Name: {event["name"]}\nDate: {date}\nLocation: {event["location"]}\n\n""")
 
 
 def clean_up_data(data: list):
+	club_names_to_remove = ["Ithaca College Nerf Club", "Ithaca College Humans Vs Zombies Social Club", "Hosted by 2 organizations", "Open Mic Night"]
 	# Clean up the data to remove the ended text.
-	filter_func = lambda line: ("Ended" not in line or "ago" not in line) and (line != "Ithaca College Nerf Club" and line != "Ithaca College Humans Vs Zombies Social Club" and line != "Hosted by 2 organizations")
+	filter_func = lambda line: ("Ended" not in line or "ago" not in line) and line not in club_names_to_remove
 	filtered_data = list(filter(filter_func, data))
 
 	# 0: Name
 	# 1: Date
 	# 2 Location
-	events = []
+	events = {}
 	for i in range(0, (len(filtered_data))-3, 3):
-		events.append({"name": filtered_data[i], "date": convert_date(filtered_data[i+1]), "location": filtered_data[i+2]})
+		date = convert_date(filtered_data[i+1])
+		events[date] = {"name": filtered_data[i], "location": filtered_data[i+2]}
 
 	print_events(events)
 		
@@ -89,17 +99,16 @@ def clean_up_data(data: list):
 
 def make_request():
 	session = HTMLSession()
-	page = session.get("https://ithaca.campuslabs.com/engage/organization/ithaca-college-nerf-club/events?showpastevents=true")
+	#page = session.get("https://ithaca.campuslabs.com/engage/organization/ithaca-college-nerf-club/events?showpastevents=true")
+	page = session.get("https://ithaca.campuslabs.com/engage/organization/open-mic-night/events")
 	page.html.render()
 	all_events = page.html.find("#org-event-discovery-list", first=True)
+	if all_events == None:
+		raise ValueError("No events found.")
 	clean_up_data(convert_text_to_list(all_events.text))
 			
 
 def main():
 	make_request()
-	#convert_date("Saturday, November 4 at 1:39PM EDT")
-
-	today = datetime.now()
-	print(today.year)	
-
+	
 main()	
